79c956a5b21a239104849e8ec263f499536aef03
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-07-08 14:33:57 -0400

    boilermaker purge db,unecessary npm packages and oauth

---
+++ server/api/amadeus.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+module.exports = router;
+
+/* router.get('/something', async(req,res,next) => {
+
+}) */
--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
+//put amadeus router here
+/* router.use('/amadeus', require('./amadeus')) */
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
