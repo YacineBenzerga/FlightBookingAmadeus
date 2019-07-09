5c681d1eb4a3e4a6fa137240c422ee7343b1b2aa
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-07-09 12:26:20 -0400

    feat(api) flights and hotels search routes added

---
+++ server/api/amadeus/flights.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const utils = require('../errorHandler');
+const amadeus = utils.amadeus;
+const defaultHandler = utils.defaultHandler;
+
+//Search flight from origin to destination on date
+router.get(
+  '/:from-:to-:date',
+  defaultHandler(async (req, res, next) => {
+    const flights = await amadeus.shopping.flightOffers.get({
+      origin: req.params.from,
+      destination: req.params.to,
+      departureDate: req.params.date
+    });
+    res.json(flights.data);
+  })
+);
+
+router.get(
+  '/most-traveled/:from-:date',
+  defaultHandler(async (req, res, next) => {
+    const mostTravDest = await amadeus.travel.analytics.AirTraffic.Traveled.get(
+      {
+        originCityCode: req.params.from,
+        period: req.params.date
+      }
+    );
+    res.json(maxLocTrav.data);
+  })
+);
+
+module.exports = router;
---
+++ server/api/amadeus/hotels.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const utils = require('../errorHandler');
+const amadeus = utils.amadeus;
+const defaultHandler = utils.defaultHandler;
+
+router.get(
+  '/:cityIata',
+  defaultHandler(async (req, res, next) => {
+    const hotels = await amadeus.shopping.hotelOffers.get({
+      cityCode: req.params.cityIata
+    });
+    res.json(hotels.data);
+  })
+);
+
+module.exports = router;
