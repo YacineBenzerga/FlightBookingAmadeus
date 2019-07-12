6fcbe8c114c2374202b0934fa4bf8718203e9331
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-07-12 10:49:22 -0400

    feat(flightBooking) flight search functional on FE

--- client/store/index.js
+++ client/store/index.js
@@ -2,11 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-/* import flightReducer from './reducers/flight'; */
+import flightReducer from './reducers/flight';
 import hotelReducer from './reducers/hotel';
 
 const reducer = combineReducers({
-  /* flight: flightReducer, */ hotel: hotelReducer
+  flight: flightReducer,
+  hotel: hotelReducer
 });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
