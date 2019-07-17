8c4fa49f3be43ecaf253e9a90547804a51b73f04
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-07-17 17:37:55 -0400

    feat(flightBooking) selected flights saved

--- client/components/FlightSearch/flightView.js
+++ client/components/FlightSearch/flightView.js
@@ -1,51 +1,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {
+  selectedDepFlight,
+  selectedRetFlight
+} from '../../store/reducers/flight';
 
-const FlightView = props => {
-  const flt = props.flt.offerItems[0];
-  const fltDetail1 = flt.services[0].segments[0];
-  const fltDetail2 = flt.services[0].segments[1];
-  const duration = fltDetail1.flightSegment.duration.split('T')[1];
-  var ttPrice = Number(flt.price.total) + Number(flt.price.totalTaxes);
-  const departLoc = fltDetail1.flightSegment.departure.iataCode;
-  const arrivLoc = fltDetail2
-    ? fltDetail2.flightSegment.arrival.iataCode
-    : fltDetail1.flightSegment.arrival.iataCode;
-  const stopLoc = fltDetail2
-    ? '1 stop ' + fltDetail1.flightSegment.arrival.iataCode
-    : 'non stop';
+class FlightView extends React.Component {
+  constructor() {
+    super();
+  }
 
-  //flight time(3:00pm - 5:54pm)   duration(5h 54m)(Nonstop)                Price($385) Button(Select)
-  //CareerLogo (Career)            EWR-SFO                                  roundtrip
+  render() {
+    console.log('props here', this.props.ns);
+    const flt = this.props.flt.offerItems[0];
+    const fltDetail1 = flt.services[0].segments[0];
+    const fltDetail2 = flt.services[0].segments[1];
+    const duration = fltDetail1.flightSegment.duration.split('T')[1];
+    var ttPrice = Number(flt.price.total) + Number(flt.price.totalTaxes);
+    const departLoc = fltDetail1.flightSegment.departure.iataCode;
+    const arrivLoc = fltDetail2
+      ? fltDetail2.flightSegment.arrival.iataCode
+      : fltDetail1.flightSegment.arrival.iataCode;
+    const stopLoc = fltDetail2
+      ? '1 stop ' + fltDetail1.flightSegment.arrival.iataCode
+      : 'non stop';
 
-  var totalPriceTax = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  }).format(ttPrice);
+    //flight time(3:00pm - 5:54pm)   duration(5h 54m)(Nonstop)                Price($385) Button(Select)
+    //CareerLogo (Career)            EWR-SFO                                  roundtrip
 
-  return (
-    <div key={flt.id} className="flightResults">
-      <ol>
-        <li>
-          {fltDetail1.flightSegment.departure.at.split('T')[1].split('-')[0]}
-        </li>
-        <li> {duration} </li>
-        <li> {totalPriceTax} </li>
-        <li>
-          <ol>
-            <li>
-              {departLoc} - {arrivLoc}
-            </li>
-            <li>{stopLoc}</li>
-          </ol>
-        </li>
-      </ol>
-      <button onClick={props.handleSelect}>Select</button>
-    </div>
-  );
-};
+    var totalPriceTax = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(ttPrice);
+
+    /* if (this.props.ns === 1) {
+      this.props.selectDepF(this.props.flt);
+    } else if (this.props.ns === 2) {
+      this.props.selectRetF(this.props.flt);
+    } */
+
+    return (
+      <div key={flt.id} className="flightResults">
+        <ol>
+          <li>
+            {fltDetail1.flightSegment.departure.at.split('T')[1].split('-')[0]}
+          </li>
+          <li> {duration} </li>
+          <li> {totalPriceTax} </li>
+          <li>
+            <ol>
+              <li>
+                {departLoc} - {arrivLoc}
+              </li>
+              <li>{stopLoc}</li>
+            </ol>
+          </li>
+        </ol>
+        <button id={JSON.stringify(flt)} onClick={this.props.handleSelect}>
+          Select
+        </button>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  selectDepF: flight => dispatch(selectedDepFlight(flight)),
+  selectRetF: flight => dispatch(selectedRetFlight(flight))
+});
 
 export default connect(
   null,
-  null
+  mapDispatchToProps
 )(FlightView);
