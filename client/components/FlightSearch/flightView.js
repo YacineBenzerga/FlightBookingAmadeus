325c984b12d516f0b72a3daf8581fb14bff9757e
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-07-22 20:43:23 -0400

    feat(styling) basic flightview styling functional

--- client/components/FlightSearch/flightView.js
+++ client/components/FlightSearch/flightView.js
@@ -5,108 +5,32 @@ import {
   selectedRetFlight
 } from '../../store/reducers/flight';
 import moment from 'moment';
-
-const xxx = {
-  type: 'flight-offer',
-  id: '1563763264773-1561981298',
-  offerItems: [
-    {
-      services: [
-        {
-          segments: [
-            {
-              flightSegment: {
-                departure: {
-                  iataCode: 'LGA',
-                  terminal: 'B',
-                  at: '2019-08-02T06:00:00-04:00'
-                },
-                arrival: {
-                  iataCode: 'ORD',
-                  terminal: '1',
-                  at: '2019-08-02T07:25:00-05:00'
-                },
-                carrierCode: 'UA',
-                number: '761',
-                aircraft: {
-                  code: '319'
-                },
-                operating: {
-                  carrierCode: 'UA',
-                  number: '761'
-                },
-                duration: '0DT2H25M'
-              },
-              pricingDetailPerAdult: {
-                travelClass: 'ECONOMY',
-                fareClass: 'W',
-                availability: 9,
-                fareBasis: 'WAA7AQDN'
-              }
-            },
-            {
-              flightSegment: {
-                departure: {
-                  iataCode: 'ORD',
-                  terminal: '1',
-                  at: '2019-08-02T08:05:00-05:00'
-                },
-                arrival: {
-                  iataCode: 'LAX',
-                  terminal: '7',
-                  at: '2019-08-02T10:38:00-07:00'
-                },
-                carrierCode: 'UA',
-                number: '204',
-                aircraft: {
-                  code: '753'
-                },
-                operating: {
-                  carrierCode: 'UA',
-                  number: '204'
-                },
-                duration: '0DT4H33M'
-              },
-              pricingDetailPerAdult: {
-                travelClass: 'ECONOMY',
-                fareClass: 'W',
-                availability: 9,
-                fareBasis: 'WAA7AQDN'
-              }
-            }
-          ]
-        }
-      ],
-      price: {
-        total: '304.19',
-        totalTaxes: '40.19'
-      },
-      pricePerAdult: {
-        total: '304.19',
-        totalTaxes: '40.19'
-      }
-    }
-  ]
-};
+import { carrierCodes } from '../../utils';
 
 var humanizeDur = (str1, str2) => {
-  var hh1 = Number(str1.split('H')[0]);
-  var mn1 = Number(str1.split('H')[1].split('M')[0]);
-  var hh2 = Number(str2.split('H')[0]);
-  var mn2 = Number(str2.split('H')[1].split('M')[0]);
+  var arr1 = str1.split('H');
+  var arr2 = str2.split('H');
+  var hh1 = Number(arr1[0]);
+  var mn1 = Number(arr1[1].split('M')[0]);
+  var hh2 = Number(arr2[0]);
+  var mn2 = Number(arr2[1].split('M')[0]);
   var resH = 0;
   var resM = mn1 + mn2;
   if (resM >= 60) {
-    resH = Math.floor((mn1 + mn2) / 60);
-    resM = Math.floor((mn1 + mn2) % 60);
+    resH = Math.floor(resM / 60);
+    resM = Math.floor(resM % 60);
   }
   const obj = { minutes: resM, hours: hh1 + hh2 + resH };
   return obj;
 };
 
 var durFix = str1 => {
-  var hh1 = Number(str1.split('h')[0]);
-  var mn1 = Number(str1.split('h')[1].split('m')[0]);
+  var arr = str1
+    .trim()
+    .toLowerCase()
+    .split('h');
+  var hh1 = Number(arr[0]);
+  var mn1 = Number(arr[1].split('m')[0]);
   const obj = { minutes: mn1, hours: hh1 };
   return obj;
 };
@@ -117,17 +41,19 @@ class FlightView extends React.Component {
   }
 
   render() {
-    const flt = xxx.offerItems[0];
+    const flt = this.props.flt.offerItems[0];
     const fltDetail1 = flt.services[0].segments[0];
-    const fltDetail2 = flt.services[0].segments[1];
+    var fltDetail2 = null;
+    if (flt.services[0].segments.length > 1) {
+      var fltDetail2 = flt.services[0].segments[1];
+    }
+
     var ttPrice = Number(flt.price.total) + Number(flt.price.totalTaxes);
     const departLoc = fltDetail1.flightSegment.departure.iataCode;
     const arrivLoc = fltDetail2
       ? fltDetail2.flightSegment.arrival.iataCode
       : fltDetail1.flightSegment.arrival.iataCode;
-    const stopLoc = fltDetail2
-      ? '1 stop ' + fltDetail1.flightSegment.arrival.iataCode
-      : 'non stop';
+    const nstop = fltDetail2 ? '1 stop ' : 'non stop';
 
     var availability = fltDetail1.pricingDetailPerAdult.availability;
     var departTime = moment(
@@ -139,9 +65,11 @@ class FlightView extends React.Component {
         fltDetail1.flightSegment.duration.split('T')[1],
         fltDetail2.flightSegment.duration.split('T')[1]
       );
-      var duration = `${resDur.hours}h ${resDur.minutes}m`;
+      var duration = `${resDur.hours}h${resDur.minutes}m`;
     } else {
-      var duration = fltDetail1.flightSegment.duration.split('T')[1];
+      var duration = fltDetail1.flightSegment.duration
+        .split('T')[1]
+        .toLowerCase();
     }
 
     var objDur = null;
@@ -161,7 +89,9 @@ class FlightView extends React.Component {
       currency: 'USD'
     }).format(ttPrice);
 
-    /* if (this.props.ns === 1) {
+    var carrier = carrierCodes[fltDetail1.flightSegment.carrierCode];
+
+    /*  if (this.props.ns === 1) {
       this.props.selectDepF(this.props.flt);
     } else if (this.props.ns === 2)
       this.props.selectRetF(this.props.flt);
@@ -171,14 +101,26 @@ class FlightView extends React.Component {
       <div key={flt.id} className="flight-view">
         <div className="flt-detail1">
           <label>
-            {departTime.format('hh:mma')}-{arrivalTime}
+            {departTime.format('hh:mma')} - {arrivalTime}
           </label>
-          <label>{duration}</label>
+          <label>{carrier}</label>
         </div>
         <div className="flt-detail2">
           <label>
-            {departLoc} -> {stopLoc} -> {arrivLoc}
+            {duration} ({nstop})
           </label>
+          {fltDetail2 ? (
+            <label>
+              {departLoc} -> {fltDetail1.flightSegment.arrival.iataCode} ->{' '}
+              {arrivLoc}
+            </label>
+          ) : (
+            <label>
+              {departLoc} -> {arrivLoc}
+            </label>
+          )}
+
+          <label />
         </div>
         <div className="flt-detail3">
           <label style={{ color: 'red' }}>
