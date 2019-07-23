4940d122dd7361cce9e5fb2d3f294be19e78af4b
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-07-22 23:28:24 -0400

    feat(styling) navbar restyle del swipeable views

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,59 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import SwipeableViews from 'react-swipeable-views';
 import { FlightFilter, HotelSearch } from './index';
-
-const styles = theme => ({
-  root: {
-    width: 800,
-    height: 1500
-  }
-});
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-  dir: PropTypes.string.isRequired
-};
-
-function TabContainer({ children, dir }) {
-  return (
-    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
-      {children}
-    </Typography>
-  );
-}
 
 class Navbar extends React.Component {
   constructor() {
     super();
-    this.state = {
-      value: 0
-    };
+    this.state = {};
   }
 
-  handleChangeTab = (event, value) => {
-    this.setState({ value });
-  };
-
-  handleChangeTabIndex = index => {
-    this.setState({ value: index });
-  };
-
   render() {
-    const { value } = this.state;
-    const { theme } = this.props;
     return (
       <div>
         <div className="w3-bar w3-white w3-border-bottom w3-xlarge">
           <a
-            href="/"
+            href="/flights"
             className="w3-bar-item w3-button w3-text-red w3-hover-red"
           >
             <b>
@@ -62,42 +24,23 @@ class Navbar extends React.Component {
             </b>
           </a>
         </div>
-        <div className="navbar-container">
+        <div>
           <header>
-            {/*  <img
+            {/* <img
               className="w3-image"
               src="traveler.jpg"
               alt="London"
               width="1500"
               height="700"
-            /> */}
-            <div>
-              <div className="w3-display-middle" style={{ width: '75%' }}>
-                <AppBar position="static" color="default">
-                  <Tabs
-                    value={value}
-                    onChange={this.handleChangeTab}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    variant="fullWidth"
-                  >
-                    <Tab label="Flights" />
-                    <Tab label="Hotels" />
-                  </Tabs>
-                </AppBar>
-                <SwipeableViews
-                  axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                  index={this.state.value}
-                  onChangeIndex={this.handleChangeIndex}
-                >
-                  <TabContainer dir={theme.direction}>
-                    <FlightFilter />
-                  </TabContainer>
-                  <TabContainer dir={theme.direction}>
-                    <HotelSearch />
-                  </TabContainer>
-                </SwipeableViews>
-              </div>
+            />  */}
+
+            <div className="topnav">
+              <a className="active">
+                <Link to="/flights">Flights</Link>
+              </a>
+              <a>
+                <Link to="/hotels">Hotels</Link>
+              </a>
             </div>
           </header>
         </div>
@@ -106,9 +49,4 @@ class Navbar extends React.Component {
   }
 }
 
-Navbar.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
-};
-
-export default withStyles(styles, { withTheme: true })(Navbar);
+export default Navbar;
